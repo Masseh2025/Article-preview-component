9200a2ec28a9b7573151b8ca829cdbdc9089dc60
mas <mas154572@gmail.com>
2024-07-24 18:10:16 -0400

    Finsihed responsive footer

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,6 +1,17 @@
 const shareButton = document.getElementById("card__share");
 const toolTip = document.querySelector(".card__tool-tip");
+const cardFooter = document.querySelector(".card__footer");
+const cardBottom = document.querySelector(".card__bottom");
+const card = document.querySelector(".card");
 
 shareButton.addEventListener("click", function () {
-  toolTip.classList.toggle("tool-tip--invisible");
+  if (getComputedStyle(card).flexDirection === "row") {
+    toolTip.classList.toggle("invisible");
+    cardFooter.classList.toggle("invisible");
+    cardBottom.classList.toggle("card__bottom--blue");
+  } else {
+    cardFooter.classList.toggle("invisible");
+    toolTip.classList.toggle("invisible");
+    cardBottom.classList.toggle("card__bottom--blue");
+  }
 });
